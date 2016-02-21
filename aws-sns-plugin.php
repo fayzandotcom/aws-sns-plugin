8667e000318ed6879dd27ad0ee88919d159ca519
Fayzan Siddiqui <fayzandotcom@hotmail.com>
2016-02-22 02:07:02 +0800

    change slug

--- aws-sns-plugin.php
+++ aws-sns-plugin.php
@@ -21,7 +21,7 @@
  * Author URI:        https://my.linkedin.com/in/fayzansiddiqui
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       aws-sns-plugin
+ * Text Domain:       aws-sns
  * Domain Path:       /languages
  */
 
